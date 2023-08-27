b4bcfa24e416199c46b50a3cd2a6af193c0b06e3
betamkd <philip-sk@hotmail.com>
2023-08-27 20:34:08 +0200

    External Api logic

--- ProjectOnlineMovieStoreAPI/Controllers/HomeController.cs
+++ ProjectOnlineMovieStoreAPI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectOnlineMovieStoreAPI.Models;
 using System.Security.Cryptography.X509Certificates;
 
 namespace ProjectOnlineMovieStoreAPI.Controllers
@@ -7,10 +9,43 @@ namespace ProjectOnlineMovieStoreAPI.Controllers
 	public class HomeController : BaseAPIController
 	{
 		[HttpGet("Hollywood")]
-		public void GetHollywood()
+		public async Task<ActionResult<IEnumerable<HollywoodsData>>> GetHollywoods()
 		{
-
-			
+			try
+			{
+				var hollywoodsResult = await CallExternalApiHollywoods();
+				
+				return Ok(hollywoodsResult);
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
 		}
-	}	
+
+
+		#region Helper Methods
+
+		public async Task<List<Hollywood>> CallExternalApiHollywoods()
+		{
+			List<Hollywood> hollywoodsData = new List<Hollywood>();
+
+			// TODO: logic for getting external data here
+			using (var httpClient = new HttpClient())
+			{
+				using (var response = await httpClient.GetAsync("https://api.themoviedb.org/3/movie/550?api_key=1e2f1f0c67745c9a8e6cb8e7f4727fe7"))
+				{
+				 string apiResponse = await response.Content.ReadAsStringAsync();
+					
+					if (!string.IsNullOrEmpty(apiResponse)) 
+					{
+						hollywoodsData = JsonConvert.DeserializeObject<List<Hollywood>>(apiResponse);
+					}
+				}
+			}
+			return hollywoodsData;
+		}
+
+		#endregion
+	}
 }
