298b85221e890a2ee8c0019059588a68cab5599e
Filip Gjurcheski <philipgjurcheski@gmail.com>
2023-10-17 20:36:44 +0200

    external api conflict - resolved

--- ProjectOnlineMovieStoreAPI/Program.cs
+++ ProjectOnlineMovieStoreAPI/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddCors(c => c.AddPolicy("OnlineMoviestoreCorsPolicy", builder =>
 {
     builder
